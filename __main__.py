765f1ca95ecf707113e0e4b8dac95363ba86d907
smallmthiro <76735321+smallmthiro@users.noreply.github.com>
2021-01-31 12:47:45 +0000

    Enabled to cut out the specified range.

--- __main__.py
+++ __main__.py
@@ -46,6 +46,30 @@ def main(args: typing.List[str]):
     else:
         raise ValueError('A non-existent model type has been set.')
 
+    x_cut_out_range_min: float = 0.0
+    if 4 > len(args):
+        x_cut_out_range_min = float(
+            input('Specify the range to cut out x.(min) : '))
+    else:
+        x_cut_out_range_min = float(args[3])
+
+    x_cut_out_range_max: float = 0.0
+    if 5 > len(args):
+        x_cut_out_range_max = float(
+            input('Specify the range to cut out x.(max) : '))
+    else:
+        x_cut_out_range_max = float(args[4])
+
+    x_cut_out_range_div: int = 0
+    if 6 > len(args):
+        x_cut_out_range_div = int(
+            input('Specify the number to divide the cut out range. : '))
+    else:
+        x_cut_out_range_div = int(args[5])
+
+    if x_cut_out_range_div <= 1:
+        raise ValueError('The number of divisions is 1 or less.')
+
     x: np.ndarray = input_data[:, 0]
     y: np.ndarray = input_data[:, 1]
 
@@ -58,8 +82,8 @@ def main(args: typing.List[str]):
         raise ValueError('A non-existent model type has been set.')
 
     solver: curvefitter.Solver = curvefitter.Solver(model_func)
-
     curve_func: curvefitter.ICurveFunc = solver.get_curve_func(x, y)
+
     estimated_y: np.ndarray = curve_func(x)
 
     estimate_label: str = ''
@@ -72,10 +96,22 @@ def main(args: typing.List[str]):
     else:
         raise ValueError('A non-existent model type has been set.')
 
+    y_cut_out_range_min: float = curve_func(np.array([x_cut_out_range_min]))[0]
+    y_cut_out_range_max: float = curve_func(np.array([x_cut_out_range_max]))[0]
+    y_cut_out_range_step: float = (
+        y_cut_out_range_max - y_cut_out_range_min) / float(x_cut_out_range_div - 1)
+
+    y_cut_out_range: np.ndarray = np.array(
+        [y_cut_out_range_step * float(i) + y_cut_out_range_min for i in reversed(range(x_cut_out_range_div))])
+    x_cut_out_range: np.ndarray = curve_func.inverse(y_cut_out_range)
+
+    plt.subplots_adjust(bottom=0.275)
     plt.scatter(x, y, s=5, color='red', alpha=0.7, label='test data')
     plt.plot(x, estimated_y, color='blue', alpha=0.7, label=estimate_label)
+    plt.scatter(x_cut_out_range, y_cut_out_range, s=10, marker='x',
+                color='black', alpha=1.0, label='cut out')
     plt.grid(True)
-    plt.legend(loc="upper right")
+    plt.legend(bbox_to_anchor=(0, -0.1), loc='upper left')
 
     plt.show()
 
